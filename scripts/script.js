d30894e945d459fa5d7065cdd11b739f3a50ee2f
Maria Gryn <maria.grin2003@gmail.com>
2022-05-04 12:51:48 +0300

    done

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function submit() {
+    const flName = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+    alert('Hello');
+    //alert(`Name: ${flName}\nEmail: ${email}\nPhone: ${phone}`);
+}
